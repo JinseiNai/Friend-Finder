eb949123103e5ff1a72153534424723e12d2e9eb
Jinsei <jinseinai@gmail.com>
2018-08-11 17:55:08 -0700

    Logic works

---
+++ app/data/friends.js
@@ -0,0 +1,25 @@
+let usersArr = [
+    {
+        name: "Marrissa Bott",
+        photo: "http://dummyimage.com/236x209.bmp/cc0000/ffffff",
+        scores: [2, 1, 3, 5, 2, 3, 4, 2, 2, 3]
+      }, {
+        name: "Vladamir Laughtisse",
+        photo: "http://dummyimage.com/213x163.bmp/5fa2dd/ffffff",
+        scores: [2, 1, 2, 5, 2, 3, 2, 5, 1, 3]
+      }, {
+        name: "Belicia Spurnier",
+        photo: "http://dummyimage.com/167x170.bmp/ff4444/ffffff",
+        scores: [1, 2, 2, 2, 2, 3, 2, 2, 2, 3]
+      }, {
+        name: "Chery Gibbons",
+        photo: "http://dummyimage.com/171x171.png/ff4444/ffffff",
+        scores: [2, 3, 3, 5, 5, 3, 5, 2, 1, 3]
+      }, {
+        name: "Jammie MacQueen",
+        photo: "http://dummyimage.com/139x155.bmp/ff4444/ffffff",
+        scores: [1, 1, 3, 3, 2, 3, 2, 2, 2, 3]
+      }
+];
+
+module.exports = usersArr;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,41 @@
+let users = require("../data/friends.js");
+
+module.exports = function (app) {
+    app.get("/api/friends", function (req, res) {
+        res.json(users);
+    });
+
+    app.post("/api/friends", function (req, res) {
+        let newUser = req.body;
+
+        let newUserScore = newUser.scores;
+
+        // Set variables for friend match
+        let friendName;
+        let friendImage;
+        // Set to high score difference to make comparison
+        let totalDifference = 100;
+
+        // Sort through the list of users array
+        for (let i = 0; i < users.length; i++) {
+            // Set variable for score differential
+            let difference = 0;
+
+            for (let k = 0; k < newUserScore.length; k++) {
+                difference += Math.abs(parseInt(users[i].scores[k]) - parseInt(newUserScore[k]));
+            }
+            if (difference < totalDifference) {
+                totalDifference = difference;
+                friendName = users[i].name;
+                friendImage = users[i].photo;
+            }
+        }
+
+        // If match difference is less than totalDifference, set match
+        console.log("Your match is " + friendName + " " + friendImage);
+
+        users.push(newUser);
+
+        res.json(newUser);
+    })
+}
